a4a6688d4a9f5c4fa9bf31d3b2ca65e72157cd7d
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2023-04-13 17:45:32 +0530

    Code Refactoring : Genres API

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+const genres = require('./routes/genres')
 const morgan = require('morgan')
-const Joi = require('joi')
 const express = require('express')
 const app = express()
 const log = require('./logger')
 const auth = require('./auth')
 const config = require('config')
 
-
-console.log(config.get('PORT'));
 app.use(express.json())
 console.log('NODE_ENV : ', process.env.NODE_ENV)
 console.log('PORT : ', process.env.PORT)
@@ -17,95 +15,12 @@ if (app.get('env') == 'development' || 'test') {
   app.use(morgan('dev'))
 }
 app.use(express.static('public'))
+app.use('/api/genres', genres)
+app.use([log, auth]) // Multiple Middleware in Array Order 
 
-app.use(log)
 
-app.use(auth)
-
-let courses = [
-  {
-    id: 1,
-    name: 'Node'
-  },
-  {
-    id: 2,
-    name: 'Express'
-  },
-  {
-    id: 3,
-    name: 'Mongo'
-  }
-]
-
-// GET all courses
-app.get('/api/courses', (req, res) => {
-  return res.status(200).send(courses)
-})
-
-// GET course by id
-app.get('/api/courses/:id', (req, res) => {
-  // Find the course with given id
-  const course = courses.find(c => c.id === parseInt(req.params.id))
-
-  // if course not found response 404
-  if (!course) return res.status(404).send('Course not found!')
-
-  // else send the course with 200
-  return res.status(200).send(course)
-})
-
-// POST : Create a course
-app.post('/api/courses', (req, res) => {
-  // Never Trust Client Input :)
-  // Data Validation
-  // if(!req.body.name || req.body.name.length < 3){
-  const { error } = validateCourse(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  }
-
-  courses.push(course)
-
-  return res.status(201).send(courses)
-})
-
-// PUT : Update a course
-app.put('/api/courses/:id', (req, res) => {
-  //  Data Validation Using Joi
-  const { error } = validateCourse(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
-
-  const course = courses.find(c => c.id === parseInt(req.params.id))
-  if (!course) return res.status(404).send('Course not found!')
-
-  course.name = req.body.name
-
-  return res.send(courses)
-})
-
-// DELETE : Delete a Coursepost
-app.delete('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id))
-  if (!course) return res.status(404).send('Course not found!')
-
-  courses.splice(req.params.id - 1, 1)
-  return res.send(courses)
-})
-
-const PORT = process.env.PORT || 3000
-// console.log('PORT : ', PORT)
+console.log("config.get('PORT') : ", config.get('PORT'))
+const PORT = config.get('PORT') || 3000
 app.listen(PORT, () => {
   console.log(`App Listening to PORT ${PORT} `)
 })
-
-// Using Joi package to validate course object
-function validateCourse (course) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required()
-  })
-
-  return schema.validate({ name: course.name })
-}
---
+++ routes/genres.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const router = express.Router()
+const Joi = require('joi')
+
+let genres = [
+  {
+    id: 1,
+    name: 'Node'
+  },
+  {
+    id: 2,
+    name: 'Express'
+  },
+  {
+    id: 3,
+    name: 'Mongo'
+  }
+]
+
+// GET all genres
+router.get('/', (req, res) => {
+  return res.status(200).send(genres)
+})
+
+// GET genre by id
+router.get('/:id', (req, res) => {
+  // Find the genre with given id
+  const genre = genres.find(c => c.id === parseInt(req.params.id))
+
+  // if genre not found response 404
+  if (!genre) return res.status(404).send('genre not found!')
+
+  // else send the genre with 200
+  return res.status(200).send(genre)
+})
+
+// POST : Create a genre
+router.post('/', (req, res) => {
+  // Never Trust Client Input :)
+  // Data Validation
+  // if(!req.body.name || req.body.name.length < 3){
+  const { error } = validateGenre(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  }
+
+  genres.push(genre)
+
+  return res.status(201).send(genres)
+})
+
+// PUT : Update a genre
+router.put('/:id', (req, res) => {
+  //  Data Validation Using Joi
+  const { error } = validateGenre(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  const genre = genres.find(c => c.id === parseInt(req.params.id))
+  if (!genre) return res.status(404).send('genre not found!')
+
+  genre.name = req.body.name
+
+  return res.send(genres)
+})
+
+// DELETE : Delete a genre
+router.delete('/:id', (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id))
+  if (!genre) return res.status(404).send('genre not found!')
+
+  genres.splice(req.params.id - 1, 1)
+  return res.send(genres)
+})
+
+// Using Joi package to validate genre object
+function validateGenre (genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  })
+
+  return schema.validate({ name: genre.name })
+}
+
+module.exports = router
