fb34f0a2af29f5ee647a1850907833f71cbdcb53
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2023-04-24 13:24:12 +0530

    CRUD implemented between client and DB Directly: Readme file Updated with Doc URL of APP

---
+++ readme.md
@@ -0,0 +1,2 @@
+Documentation for Kaanoli App :
+https://documenter.getpostman.com/view/21337972/2s93Y5PzjR
--- routes/genres.js
+++ routes/genres.js
@@ -6,6 +6,12 @@ const config = require('config')
 
 console.log(`config.get('db_Uri') : ${config.get('db_Uri')}`)
 
+const genreSchema = new mongoose.Schema({
+  name: String
+})
+
+const Genre = mongoose.model('Genre', genreSchema)
+
 mongoose
   .connect(config.get('db_Uri'), {
     useNewUrlParser: true,
@@ -20,77 +26,93 @@ mongoose
     process.exit(1)
   })
 
-let genres = [
-  {
-    id: 1,
-    name: 'Node'
-  },
-  {
-    id: 2,
-    name: 'Express'
-  },
-  {
-    id: 3,
-    name: 'Mongo'
-  }
-]
+// let genres = [
+//   {
+//     id: 1,
+//     name: 'Node'
+//   },
+//   {
+//     id: 2,
+//     name: 'Express'
+//   },
+//   {
+//     id: 3,
+//     name: 'Mongo'
+//   }
+// ]
 
 // GET all genres
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name')
   return res.status(200).send(genres)
 })
 
 // GET genre by id
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // Find the genre with given id
-  const genre = genres.find(c => c.id === parseInt(req.params.id))
+  // const genres = genres.find(c => c.id === parseInt(req.params.id))
+  const genre = await Genre.findById(req.params.id)
 
   // if genre not found response 404
   if (!genre) return res.status(404).send('genre not found!')
 
   // else send the genre with 200
+  console.log('genre : ', genre)
+  console.log('POST type of :', typeof genre)
+  console.log('Strin type of :', genre.toString())
+  console.log('Joson type of :', JSON.stringify(genre))
+
   return res.status(200).send(genre)
 })
 
 // POST : Create a genre
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // Never Trust Client Input :)
   // Data Validation
   // if(!req.body.name || req.body.name.length < 3){
+
   const { error } = validateGenre(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
-  const genre = {
-    id: genres.length + 1,
+  const genre = await new Genre({
     name: req.body.name
-  }
+  }).save()
 
-  genres.push(genre)
+  // genres.push(genre)
+  await genre.save()
 
-  return res.status(201).send(genres)
+  console.log('genre POST :', genre)
+  console.log('POST type of :', typeof genre)
+
+  return res.status(201).send(genre)
 })
 
 // PUT : Update a genre
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   //  Data Validation Using Joi
   const { error } = validateGenre(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
-  const genre = genres.find(c => c.id === parseInt(req.params.id))
+  // genre.name = req.body.name
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name
+    },
+    { new: true }
+  )
+
   if (!genre) return res.status(404).send('genre not found!')
 
-  genre.name = req.body.name
-
-  return res.send(genres)
+  return res.status(202).send(genre)
 })
 
 // DELETE : Delete a genre
-router.delete('/:id', (req, res) => {
-  const genre = genres.find(c => c.id === parseInt(req.params.id))
+router.delete('/:id', async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id)
   if (!genre) return res.status(404).send('genre not found!')
 
-  genres.splice(req.params.id - 1, 1)
-  return res.send(genres)
+  return res.status(200).send(genre)
 })
 
 // Using Joi package to validate genre object
